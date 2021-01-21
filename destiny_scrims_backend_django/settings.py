b67bde6c01bd120831665c8796b746b8f99e99f0
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2021-01-21 10:56:39 -0500

    [settings] Update Settings

--- destiny_scrims_backend_django/settings.py
+++ destiny_scrims_backend_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 from pathlib import Path
+from keys import DJANGO_SECRET
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ojh&55c_me$2+%n5s7sgyjs54kln1uthus_cn&j+i*zz$b63c('
+SECRET_KEY = DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
