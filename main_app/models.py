f1742fb1129afb47363fccf40b7c0a280fedd46a
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2021-01-21 16:16:14 -0500

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,31 @@ from djongo import models
 
 # Create your models here.
 
+
+
 class User(models.Model):
     member_id = models.CharField(max_length=100)
     access_token= models.CharField(max_length=100)
     refresh_token = models.CharField(max_length=100)
     expiry_date = models.DateTimeField
+    displayName = models.CharField
+    membershipId = models.CharField
+    membershipType = models.CharField
+    groupId = models.CharField
+    groupName = models.CharField
 
+
+
+class Team(models.Model):
+    player1 = models.CharField(max_length=100)
+    player2 = models.CharField(max_length=100)
+    player3 = models.CharField(max_length=100)
+
+
+class Tournament(models.Model):
+    member_id = models.CharField(max_length=100)
+    numTeams = models.IntegerField
+
+    
+    
+   
